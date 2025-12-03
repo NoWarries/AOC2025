56dfb3b37628ba61e4535f8db993ed4b62b18082
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2025-12-03 22:31:29 +0100

    feat(three): Implement day three part two implementation

---
+++ src/three/ThreePartTwo.kt
@@ -0,0 +1,40 @@
+package three
+
+import java.io.File
+
+fun main() {
+    val inputFile = File("src/three/input.txt")
+
+    val banks = mutableListOf<String>()
+    inputFile.forEachLine {
+        banks.add(it)
+    }
+
+    var totalJoltage = 0L
+
+    val targetLength = 12
+
+    for (bank in banks) {
+        val result = mutableListOf<Number>()
+        var currentLength = targetLength
+        var series = bank
+
+        for (number in 1..targetLength) {
+            for (i in 9 downTo 1) {
+                val index = series.indexOf("$i")
+
+                if (index == -1 || index > series.length - currentLength) {
+                    continue
+                }
+                result.add(i)
+                series = series.slice(series.indexOf("$i") + 1..<series.toList().size)
+                currentLength -= 1
+                break
+            }
+        }
+
+        totalJoltage += result.joinToString("").toLong()
+    }
+
+    println("The password is $totalJoltage")
+}
